1a14301ebdf683bb208e9f5f43ad9a74811194cb
Ego1stCompany <well02091999@gmail.com>
2024-09-29 23:56:16 +0300

    add server

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const {prisma} = require('../prisma/prisma-client')
+
+const auth = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization?.split(' ')[1]
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        
+        req.user = await prisma.user.findUnique({where: {id: decoded.id}})
+
+        next()
+    } catch (error) {
+        res.status(401).json({message: "Не авторизован"})
+    }
+}
+
+module.exports = {
+    auth
+}
